445d702fd210486dff0b6905f008f4cac8d6da15
Firdosee <aafreenaafreen2395@gmail.com>
2022-04-08 11:27:31 +0530

    adding spark report

--- test-output-thread/report.js
+++ test-output-thread/report.js
@@ -1,110 +1,110 @@
 $(document).ready(function() {
 CucumberHTML.timelineItems.pushArray([
   {
-    "id": "dcb4507b-70b6-4057-a35e-42f88e155760",
+    "id": "72ab9ab2-fe37-4e54-b582-04a9ed38132f",
     "feature": "Validate Dashboard Details",
     "scenario": "Validate MY Profile Details",
-    "start": 1649314605991,
-    "group": 50,
+    "start": 1649397170199,
+    "group": 49,
     "content": "",
     "tags": "",
-    "end": 1649314624900,
+    "end": 1649397202030,
     "className": "passed"
   },
   {
-    "id": "fe957b72-7ac0-4c86-aac9-fa2795082220",
-    "feature": "Validate Dashboard Details",
-    "scenario": "Validate that the logo is present across website",
-    "start": 1649314605998,
-    "group": 53,
-    "content": "",
-    "tags": "",
-    "end": 1649314627093,
-    "className": "passed"
-  },
-  {
-    "id": "829164b6-ba6f-4965-8530-0e5347ad889f",
-    "feature": "Validate Dashboard Details",
-    "scenario": "Validate links in the Footer across website",
-    "start": 1649314606007,
-    "group": 52,
-    "content": "",
-    "tags": "",
-    "end": 1649314626542,
-    "className": "passed"
-  },
-  {
-    "id": "48223202-4b26-47da-83dc-71351354f110",
-    "feature": "Validate Dashboard Details",
-    "scenario": "Validate New Car \u0026 Used car exists across all webpages",
-    "start": 1649314606013,
-    "group": 54,
-    "content": "",
-    "tags": "",
-    "end": 1649314627052,
-    "className": "passed"
-  },
-  {
-    "id": "70906296-83cc-4d6e-a6d0-3285095743d5",
-    "feature": "Validate Dashboard Details",
-    "scenario": "Validate Search functionality",
-    "start": 1649314606027,
-    "group": 51,
-    "content": "",
-    "tags": "",
-    "end": 1649314656418,
-    "className": "failed"
-  },
-  {
-    "id": "1bc3e00a-9890-437a-aee4-fe33f9b4085c",
+    "id": "89500ef1-5893-4fbe-8390-a8a7c52f59e1",
     "feature": "Validate Dashboard Details",
     "scenario": "Validate the on click of logo , user is taken to homepage.",
-    "start": 1649314606004,
-    "group": 56,
-    "content": "",
-    "tags": "",
-    "end": 1649314627499,
-    "className": "passed"
-  },
-  {
-    "id": "6407bf58-8f29-42c1-a0cf-03e096ac9b92",
-    "feature": "Validate Dashboard Details",
-    "scenario": "Validate Find your car , popular ,Just launched,upcoming static elements appear in home page",
-    "start": 1649314606016,
+    "start": 1649397170192,
     "group": 55,
     "content": "",
     "tags": "",
-    "end": 1649314624353,
+    "end": 1649397214416,
     "className": "passed"
+  },
+  {
+    "id": "c3692a59-3063-40e9-92a9-95a0a4282473",
+    "feature": "Validate Dashboard Details",
+    "scenario": "Validate Find your car , popular ,Just launched,upcoming static elements appear in home page",
+    "start": 1649397170206,
+    "group": 54,
+    "content": "",
+    "tags": "",
+    "end": 1649397196315,
+    "className": "passed"
+  },
+  {
+    "id": "fd611306-8cff-4264-ba4c-c79596ef1ecd",
+    "feature": "Validate Dashboard Details",
+    "scenario": "Validate that the logo is present across website",
+    "start": 1649397170186,
+    "group": 52,
+    "content": "",
+    "tags": "",
+    "end": 1649397204409,
+    "className": "passed"
+  },
+  {
+    "id": "1f2747cc-3e37-4e05-878b-e520a7548304",
+    "feature": "Validate Dashboard Details",
+    "scenario": "Validate New Car \u0026 Used car exists across all webpages",
+    "start": 1649397170213,
+    "group": 53,
+    "content": "",
+    "tags": "",
+    "end": 1649397201092,
+    "className": "passed"
+  },
+  {
+    "id": "99435cae-601a-4c2f-98f8-0998a3971a09",
+    "feature": "Validate Dashboard Details",
+    "scenario": "Validate links in the Footer across website",
+    "start": 1649397170205,
+    "group": 51,
+    "content": "",
+    "tags": "",
+    "end": 1649397214842,
+    "className": "passed"
+  },
+  {
+    "id": "381f635f-e526-4ea3-9d21-3aae6ef9bc5a",
+    "feature": "Validate Dashboard Details",
+    "scenario": "Validate Search functionality",
+    "start": 1649397170198,
+    "group": 50,
+    "content": "",
+    "tags": "",
+    "end": 1649397235581,
+    "className": "failed"
   }
 ]);
 CucumberHTML.timelineGroups.pushArray([
   {
-    "id": 50,
+    "id": 49,
     "content": "Thread[TestNG-PoolService-0,5,main]"
   },
   {
-    "id": 51,
+    "id": 50,
     "content": "Thread[TestNG-PoolService-1,5,main]"
   },
   {
-    "id": 52,
+    "id": 51,
     "content": "Thread[TestNG-PoolService-2,5,main]"
   },
   {
-    "id": 53,
+    "id": 52,
     "content": "Thread[TestNG-PoolService-3,5,main]"
   },
   {
-    "id": 54,
+    "id": 53,
     "content": "Thread[TestNG-PoolService-4,5,main]"
   },
   {
-    "id": 55,
+    "id": 54,
     "content": "Thread[TestNG-PoolService-5,5,main]"
   },
   {
-    "id": 56,
+    "id": 55,
     "content": "Thread[TestNG-PoolService-6,5,main]"
   }
 ]);
